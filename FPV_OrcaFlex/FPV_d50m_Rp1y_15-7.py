00cccec58e5ebcbf21066580d9f8903b6c092d89
hojjatanvari <164670568+hojjatanvari@users.noreply.github.com>
2024-03-23 15:33:08 -0500

    Add files via upload

---
+++ FPV_OrcaFlex/FPV_d50m_Rp1y_15-7.py
@@ -0,0 +1,18 @@
+# Program: OrcaFlex 10.0e
+# User: Hojjat Anvari
+# Machine: DESKTOP-UAN71MS
+# Depth = 50 m & Return period = 1 yeaar - 15*7
+import OrcFxAPI as orca
+model = orca.Model()
+model.LoadData("FSPV_d50m_Rp1y_15-7.dat")  # binary data file
+general = model.general
+#----------------------------------------------------------------------------
+
+
+
+
+# Codes and results of this load case and other load cases will be uploaded once the paper is published.
+
+
+
+
